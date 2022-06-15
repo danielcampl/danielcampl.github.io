cf0c62be896de28f56fca361ef7483d95c894968
Daniel Campelo <danielcampelo159@gmail.com>
2022-06-15 14:25:04 -0300

    Adiciona esqueleto do portfólio

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-//window.onload = function initial() {
-//    const color = document.querySelector('.color');
-//    color.classList.add('selected');
-//};
-
 const backInpt = document.getElementsByClassName('color');
 
 for (let index = 0; index <= backInpt.length; index += 1) {
-    backInpt[index].style.backgroundColor = 'black';
-    backInpt[index].style.borderColor = '#ddbea8';
-    backInpt[index].addEventListener('click', changeBack = (event) => {
-
-        //let changeClass = document.querySelector('.selected');
-        //changeClass.classList.remove('selected');
-        //event.target.classList.add('selected');
-
-        if (backInpt[index].style.backgroundColor === 'black') {
-            event.target.style.backgroundColor = '#ddbea8';
-        }
-        if (backInpt[index].style.backgroundColor === '#ddbea8') {
-            event.target.style.backgroundColor = 'black';
-        }
-    })
+    backInpt[index].style.backgroundColor = '#f3dea4';
+    backInpt[index].style.border = '2px solid #1b1727';
 }
+
